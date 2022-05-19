45200886eb5e7a437e026473435a4d16b120b56a
Kevin Kutzner <kevin.kutzner@hft-stuttgart.de>
2022-05-19 13:41:56 +0200

    setup project

---
+++ src/main/java/com/kebidge/hellogke/HelloGkeApplication.java
@@ -0,0 +1,13 @@
+package com.kebidge.hellogke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGkeApplication.class, args);
+	}
+
+}
